ad560e8fb5bc66eaeefb1673cd72d4fe86d142e6
Keanu Rivers <madlad@Keanus-MacBook-Pro.local>
2022-07-20 11:19:11 -0700

    added questions and answers in Javascript

---
+++ assets/script.js
@@ -0,0 +1,37 @@
+const quizData = [
+    {
+        question: "What is the skeleton of a Web Browser?",
+        a: "Javascript",
+        b: "Python",
+        c: "HTML",
+        d: "CSS",
+        correct: "c"
+    },
+    {
+        question: "Arrays in Javascript can be used to store what?"
+        a: "Other Arrays"
+        b: "Booleans",
+        c: "Numbers and Strings",
+        d: "All of the above",
+        correct:"d"
+    },
+    {
+        question: "How many looping statements are in Javascript?"
+        a: "7",
+        b: "20",
+        c: "1",
+        d: "15",
+        correct:"a"
+    },
+    {
+        question: "What does CSS stand for?"
+        a: "Central Style Sheet"
+        b: "Cascading Style Sheet"
+        c: "Cat Smile Sinister"
+        d: "Cascading Simple Sheets"
+        correct:"b"
+    }
+
+
+
+]
