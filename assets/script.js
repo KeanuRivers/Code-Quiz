755ca9e8617746e14512193f51cc5089c73c0284
Keanu Rivers <madlad@Keanus-MacBook-Pro.local>
2022-07-21 14:48:22 -0700

    added more buttons and a bonus question

--- assets/script.js
+++ assets/script.js
@@ -1,134 +1,46 @@
-var buttonEl = document.querySelector("#start");
-var timer = document.querySelector("#timer");
-var startContainer = document.getElementById("game-content")
-var questionDisplay = document.getElementById("questions");
-var choice1 = document.getElementById("question1");
-var choice2 = document.getElementById("question2");
-var choice3 = document.getElementById("question3");
-var choice4 = document.getElementById("question4");
-var results = document.querySelector("#results");
-var highScore = JSON.parse(localStorage.getItem("quiz")) || [];
-var win = document.getElementById("win");
-var input = document.getElementById("initials");
-var saveBtn = document.getElementById("save");
-// var localStorage = window.localStorage;
-var score = 0;
-var wins = 0;
-var losses = 0;
-var counter = 20;
-var questionTimer = " ";
-var questions = [
+let quiz = document.getElementById('quiz');
+let questionContent = document.getElementById('question');
+let choice1 = document.getElementById('question1');
+let choice2 = document.getElementById('question2');
+let choice3 = document.getElementById('question3');
+let choice4 = document.getElementById('question4');
+let saveBtn = document.getElementById('save');
+let results = document.querySelector('#results');
+let win = document.getElementById('win');
+let input = document.getElementById('initials');
+let highScore = JSON.parse(localStorage.getItem('quiz')) || [];
+let startBtn = document.getElementById('start');
+
+let score = 0;
+let wins = 0;
+let losses = 0;
+let counter = 20;
+let questionTimer = " ";
+
+let questions = [
     {
-        question: "What is the base structure for web design?", 
+        question: "What is the skeleton of a Web Browser?",
         choices: ["HTML", "CSS", "JavaScript", "jQuery"],
         answers: "HTML"
     },
     {
-        question: "What is one of the most complicated development language?", 
-        choices: ["HTML", "CSS", "JavaScript", "jQuery"],
-        answers: "JavaScript"
+        question: "Arrays in Javascript can be used to store what?",
+        choices: ["Other Arrays", "Booleans", "Numbers and Strings", "All of the above"],
+        answers: "All of the above"
     },
     {
-        question: "In JavaScript, the block of code that contains repeatable code or used more than once is known as a...?", 
-        choices: ["Block", "Function", "For Loop", "Console"],
-        answers: "Function"
+        question: "How many looping statements are in Javascript?",
+        choices: ["7", "20", "1", "15"],
+        answers: "7"
     },
     {
-        question: "Which form of web design is used to change the style of a page?", 
-        choices: ["HTML", "CSS", "JavaScript", "jQuery"],
-        answers: "CSS"
+        question: "What does CSS stand for?",
+        choices: ["Central Style Sheet", "Cascading Style Sheet", "Cat Smile Sinister", "Cascading Simple Sheets"],
+        answers: "Cascading Style Sheet"
     },
     {
         question: "Who is Luke Skywalker's father?", 
         choices: ["Count Dooku", "Yoda", "Obi-Wan Kenobi", "Darth Vader"],
         answers: "Darth Vader"
     }
-]
-var questionCurrent = 0;
-startContainer.style.display = "none";
-results.style.display = "none";
-
-
-// Start Button
-buttonEl.addEventListener("click", function () {
-    startContainer.style.display = "block";
-    buttonEl.style.display = "none";
-    questionTimer = setInterval(timerDisplay, 1000)
-    displayQuestion();
-});
-
-
-// Questions diplayed
-function displayQuestion() {
-    questionDisplay.textContent = questions[questionCurrent].question;
-    choice1.innerHTML = questions[questionCurrent].choices[0];
-    choice2.innerHTML = questions[questionCurrent].choices[1];
-    choice3.innerHTML = questions[questionCurrent].choices[2];
-    choice4.innerHTML = questions[questionCurrent].choices[3];
-        choice1.setAttribute("value", questions[questionCurrent].choices[0]);
-        choice2.setAttribute("value", questions[questionCurrent].choices[1]);
-        choice3.setAttribute("value", questions[questionCurrent].choices[2]);
-        choice4.setAttribute("value", questions[questionCurrent].choices[3]);
-        choice1.onclick = userChoice;
-        choice2.onclick = userChoice;
-        choice3.onclick = userChoice;
-        choice4.onclick = userChoice;
-    
-};
-
-// Timer
-function timerDisplay() {
-    if (counter > 0) {
-        timer.textContent = counter;
-        counter--;
-    }else {
-        clearInterval(questionTimer);
-        displayResults();
-    }
-}
-
-// User Chooses answers
-function userChoice () {
-    var currentChoice = this.value 
-    console.log(currentChoice);
-    if (currentChoice === questions[questionCurrent].answers){
-        wins++;
-    } else {
-        losses++;
-    }
-    if (questionCurrent < questions.length -1){
-        questionCurrent++;
-        displayQuestion();
-    } else {
-        displayResults();
-    }
-};
-
-// Results of Quiz:
-function displayResults () {
-    //hide game content div container. Add 1 more div container to dipslay results. Create a button to store high score in local storage.
-    startContainer.style.display ="none";
-    results.style.display = "block";
-    timer.style.display = "none";
-    win.textContent = "wins:"+ wins + "losses"+ losses;
-    
-
-
-    console.log(wins, losses);
-}
-
-//  Save button:
-
-saveBtn.addEventListener("click", function(){
-    var userInitials = initials.value
-    var score = {
-        id: userInitials,
-        win: wins
-    }
-    highScore.push(score);
-    localStorage.setItem("quiz", JSON.stringify(highScore));
-
-    console.log(JSON.parse(localStorage.getItem("quiz")))
-
-    window.location.assign("./score.html");
-})
+];
